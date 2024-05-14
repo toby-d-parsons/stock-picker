0fc950ae598401cf458d1111a0d0234efd5808a6
Toby Parsons <toby.d.parsons@gmail.com>
2024-05-14 14:39:27 +0100

    Refactor stock_picker method
    - Reformatted each method call on their own line for better visual clarity

--- script.rb
+++ script.rb
@@ -1,11 +1,11 @@
 require 'pry-byebug'
 
 def stock_picker(arr)
-    hash = {}
-    combos_arr = arr.combination(2).to_a
-    calc = combos_arr.each { |obj| obj.unshift(obj[1] - obj[0]) }
-    maximum = calc.max(1)[0]
-    maximum.slice(1,2)
+    arr.combination(2)
+       .to_a
+       .each { |obj| obj.unshift(obj[1] - obj[0]) }
+       .max(1)[0]
+       .slice(1,2)
 end    
 
 stock_picker([17,3,6,9,15,8,6,1,10])
